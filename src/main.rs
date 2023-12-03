262a1fae46df8a1900aaca640be5422d02c87f6f
wooningeire <22846982+wooningeire@users.noreply.github.com>
2023-12-03 04:18:17 -0500

    feat: day 3

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::env;
+
 mod d01;
 mod d02;
+mod d03;
 
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
+
     // println!("Day 1:");
     // println!("{}", d01::run_01());
     // println!("{}", d01::run_02());
     
-    println!("Day 2:");
-    println!("{}", d02::run_01());
-    println!("{}", d02::run_02());
+    // println!("Day 2:");
+    // println!("{}", d02::run_01());
+    // println!("{}", d02::run_02());
+    
+    println!("Day 3:");
+    println!("{}", d03::run_01());
+    println!("{}", d03::run_02());
 }
