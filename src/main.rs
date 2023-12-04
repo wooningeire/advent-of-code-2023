60391b657bbbc39ba1def99eb010920b5fc918c3
wooningeire <22846982+wooningeire@users.noreply.github.com>
2023-12-04 04:47:33 -0500

    feat: use cli args to specify day

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,38 @@ mod d04;
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
 
-    // println!("Day 1:");
-    // println!("{}", d01::run_01());
-    // println!("{}", d01::run_02());
-    
-    // println!("Day 2:");
-    // println!("{}", d02::run_01());
-    // println!("{}", d02::run_02());
-    
-    // println!("Day 3:");
-    // println!("{}", d03::run_01());
-    // println!("{}", d03::run_02());
-    
-    println!("Day 4:");
-    println!("{}", d04::run_01());
-    println!("{}", d04::run_02());
+    let args = env::args().collect::<Vec<_>>();
+
+    if let Some(day_number) = args.get(1) {
+        if let Ok(n_day) = day_number.parse::<i32>() {
+            match n_day {
+                1 => {
+                    println!("Day 1:");
+                    println!("{}", d01::run_01());
+                    println!("{}", d01::run_02());
+                },
+                2 => {
+                    println!("Day 2:");
+                    println!("{}", d02::run_01());
+                    println!("{}", d02::run_02());
+                },
+                3 => {
+                    println!("Day 3:");
+                    println!("{}", d03::run_01());
+                    println!("{}", d03::run_02());
+                },
+                4 => {
+                    println!("Day 4:");
+                    println!("{}", d04::run_01());
+                    println!("{}", d04::run_02());
+                },
+
+                _ => println!("Day number is out of bounds!"),
+            };
+        } else {
+            println!("Day number is not a number!");
+        }
+    } else {
+        println!("Please supply a day number as an argument!");
+    }
 }
